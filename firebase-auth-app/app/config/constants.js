eb9ee701bc6aa18e5f45200e99767aab56c252d8
Lukas Simianer <lsimianer@gmail.com>
2019-12-17 23:32:09 -0600

    on to part 3

---
+++ firebase-auth-app/app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyCoAXe4zEbJ0TKh8CkP3meilrIiwhSV_QM";
+export const FIREBASE_AUTH_DOMAIN = "gymrunner-poc.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://gymrunner-poc.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "gymrunner-poc";
+export const FIREBASE_STORAGE_BUCKET = "gymrunner-poc.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "681708002035";
+
+export const FACEBOOK_APP_ID = "1:681708002035:web:2f1d2e890545de248e3586";
